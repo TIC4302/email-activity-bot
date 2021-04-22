e6227b384f68ec3a2df24ef5e74802c94d1ea0d8
superdupercat3 <32645742+superdupercat3@users.noreply.github.com>
2021-04-22 04:21:35 -0700

    Delete download_install_script.sh

--- download_install_script.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/TIC4302/email-activity-bot/blob/main/install.sh
-wget https://github.com/TIC4302/email-activity-bot/blob/main/run_server.sh
-wget https://github.com/TIC4302/email-activity-bot/blob/main/run_bot.sh
-wget https://github.com/TIC4302/email-activity-bot/blob/main/bandit_install.sh
