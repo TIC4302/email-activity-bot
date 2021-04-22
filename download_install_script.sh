19457e52e9d1e72150fc95cefc5d2ebee1131c7a
superdupercat3 <32645742+superdupercat3@users.noreply.github.com>
2021-04-22 04:41:07 -0700

    Update download_install_script.sh

--- download_install_script.sh
+++ download_install_script.sh
@@ -2,3 +2,4 @@ wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/install.s
 wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/run_bot.sh
 wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/run_server.sh
 wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/bandit_install.sh
+wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/bandit_check.sh
