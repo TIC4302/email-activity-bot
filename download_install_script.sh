e429fb2dc4dfe26e8c13764d040f2b8f7b4f117a
superdupercat3 <32645742+superdupercat3@users.noreply.github.com>
2021-04-22 04:22:01 -0700

    Add files via upload

---
+++ download_install_script.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/install.sh
+wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/run_bot.sh
+wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/run_server.sh
+wget https://raw.githubusercontent.com/TIC4302/email-activity-bot/main/bandit_install.sh
